737be7218e3724d0452e3c0fed7c5c85f4f90d00
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-10 22:55:03 +0300

    initial commit

---
+++ models/hotel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+
+
+const hotelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    distance: {
+        type: String,
+        required: true,
+    },
+    photos: [{ type: String }],
+    title: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type:Number,
+        min:0,
+        max: 5
+    },
+    rooms: [{ type: String }],
+    cheapestPrice: {
+        type: Number,
+        required: true,
+    },
+    featured: {
+        type: Boolean,
+        default: false,
+    },
+})
+
+hotelSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Hotel', hotelSchema)
