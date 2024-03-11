074a6c4afd5c2904e4067fc6c49322b7238cf4ae
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-11 23:51:33 +0300

    initial commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
 const hotelsRouter = require('./controllers/hotels')
+const roomsRouter = require('./controllers/rooms')
 
 const app = express()
 const connect = require('./utils/db')
@@ -16,6 +17,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/hotels',hotelsRouter)
+app.use('/api/rooms',roomsRouter)
 
 app.use(middleware.unknownEndpointsHandler)
 app.use(middleware.errorHandler)
